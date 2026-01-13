7b732223e8469d96eb0d564dcff3ecf40ff76f0c
medhedibenhmida <mohamedelhedi.benhmida@esprit.tn>
2026-01-13 14:50:32 +0100

    feat(auth): add login component, form validation and login request

---
+++ src/app/service/auth/auth.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  private apiUrl = 'http://localhost:8080/users';
+  constructor(private http: HttpClient) {}
+
+  login(credentials: { email: string, password: string }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, credentials);
+  }
+
+}
